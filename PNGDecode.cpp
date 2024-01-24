b6362431443a266dafdfa96ffb2ffc527576af94
Artem Litvin <mypos@artemlitvin.com>
2024-01-24 19:03:52 +0600

    add wrapper c++

---
+++ PNGDecode.cpp
@@ -0,0 +1,2 @@
+#include "PNGDecode.hpp"
+
---
+++ PNGDecode.hpp
@@ -0,0 +1,17 @@
+#ifndef __PNGDECODE_HPP
+#define __PNGDECODE_HPP
+
+#include "lib/lodepng.h"
+
+
+class PNGDecode
+{
+public:
+
+
+private:
+
+
+};
+
+#endif
